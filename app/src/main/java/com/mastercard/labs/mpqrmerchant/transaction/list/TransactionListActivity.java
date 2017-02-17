2602a23bffb443afc2678019d7481977a3cefc23
Muhammad Azeem <muhammad.azeem@mastercard.com>
2017-02-17 10:39:10 +0800

    Update resources

--- app/src/main/java/com/mastercard/labs/mpqrmerchant/transaction/list/TransactionListActivity.java
+++ app/src/main/java/com/mastercard/labs/mpqrmerchant/transaction/list/TransactionListActivity.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
